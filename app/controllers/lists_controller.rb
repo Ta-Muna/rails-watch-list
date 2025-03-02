7b85c17544d8b107c90f7f708e6c46558b916ec0
Tamari Pilia <tamaripilia13@gmail.com>
2025-03-02 14:30:18 +0100

    added functional delete button paths

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,9 +20,15 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path(@list), notice: 'List deleted'
+  end
+
   private
+
   def list_params
     params.require(:list).permit(:name)
   end
-
 end
